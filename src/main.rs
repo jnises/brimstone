c4d1e4427c879f7596efb149cc496f3619810ea4
Joel Nises <joel.nises@gmail.com>
2022-04-19 10:54:50 +0200

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,61 @@
 use eframe::{
     egui::{self, Vec2},
-    epaint::{Color32},
+    epaint::Color32,
     epi,
 };
+use palette::{FromColor, IntoColor, Oklab, Srgb, Clamp};
+
+const IMG_SIZE: usize = 512;
 
 #[derive(Clone, PartialEq)]
 struct Params {
-    gray: u8,
+    l: f32,
+    a: f32,
+    b: f32,
 }
 
 impl Default for Params {
     fn default() -> Self {
-        Self { gray: 128 }
+        let l = (Oklab::<f32>::max_l() + Oklab::<f32>::min_l()) / 2.;
+        let a = (Oklab::<f32>::max_a() + Oklab::<f32>::min_a()) / 2.;
+        let b = (Oklab::<f32>::max_b() + Oklab::<f32>::min_b()) / 2.;
+        Self { l, a, b }
     }
 }
 
 fn make_texture_from_params(ctx: &eframe::egui::Context, params: &Params) -> egui::TextureHandle {
+    // create the buffer in a more efficient way
+    let mut buf = Box::new([0u8; IMG_SIZE * IMG_SIZE * 4]);
+    for y in 0..IMG_SIZE {
+        let normy = y as f32 / IMG_SIZE as f32;
+        for x in 0..IMG_SIZE {
+            let normx = x as f32 / IMG_SIZE as f32;
+            let lab = palette::Oklab::new(params.l, normx * 2. - 1., normy * 2. - 1.).clamp();
+            let rgb = Srgb::from_color(lab).into_format();
+            let base = 4 * (x + (IMG_SIZE * y));
+            buf[base] = rgb.red;
+            buf[base + 1] = rgb.green;
+            buf[base + 2] = rgb.blue;
+            buf[base + 3] = 0xff;
+        }
+    }
     ctx.load_texture(
         "gradient",
-        egui::ColorImage::new([512, 512], Color32::from_gray(params.gray)),
+        egui::ColorImage::from_rgba_unmultiplied([IMG_SIZE, IMG_SIZE], buf.as_ref()),
+        // egui::ColorImage::new(
+        //     [IMG_SIZE, IMG_SIZE],
+        //     Color32::from_rgb(color.red, color.green, color.blue),
+        // ),
     )
+    // let lab = palette::Oklab::new(params.l, params.a, params.b).clamp();
+    // let color = Srgb::from_color(lab).clamp().into_format();
+    // ctx.load_texture(
+    //     "gradient",
+    //     egui::ColorImage::new(
+    //         [IMG_SIZE, IMG_SIZE],
+    //         Color32::from_rgb(color.red, color.green, color.blue),
+    //     ),
+    // )
 }
 
 struct Gui {
@@ -33,23 +69,40 @@ impl Gui {
 }
 
 impl epi::App for Gui {
-    fn update(&mut self, ctx: &eframe::egui::Context, frame: &epi::Frame) {
+    fn update(&mut self, ctx: &eframe::egui::Context, _frame: &epi::Frame) {
         let mut newparams = if let Some((params, _)) = &self.texture {
             params.clone()
         } else {
             Params::default()
         };
         egui::CentralPanel::default().show(ctx, |ui| {
-            ui.add(egui::Slider::new(&mut newparams.gray, 0..=255).text("Gray"));
-            match &self.texture {
-                Some((params, _)) if params == &newparams => {}
-                _ => {
-                    let tex = make_texture_from_params(ctx, &newparams);
-                    self.texture = Some((newparams, tex));
-                }
-            }
-            let texture = &self.texture.as_ref().unwrap().1;
-            ui.image(texture, texture.size_vec2());
+            ui.horizontal(|ui| {
+                ui.vertical(|ui| {
+                    ui.add(
+                        egui::Slider::new(&mut newparams.l, Oklab::min_l()..=Oklab::max_l())
+                            .text("L"),
+                    );
+                    ui.add(
+                        egui::Slider::new(&mut newparams.a, Oklab::min_a()..=Oklab::max_a())
+                            .text("a"),
+                    );
+                    ui.add(
+                        egui::Slider::new(&mut newparams.b, Oklab::min_b()..=Oklab::max_b())
+                            .text("b"),
+                    );
+                });
+                ui.vertical(|ui| {
+                    match &self.texture {
+                        Some((params, _)) if params == &newparams => {}
+                        _ => {
+                            let tex = make_texture_from_params(ctx, &newparams);
+                            self.texture = Some((newparams, tex));
+                        }
+                    }
+                    let texture = &self.texture.as_ref().unwrap().1;
+                    ui.image(texture, texture.size_vec2());
+                });
+            });
         });
     }
 
@@ -65,8 +118,8 @@ fn main() {
         app,
         epi::NativeOptions {
             initial_window_size: Some(Vec2 {
-                x: 600f32,
-                y: 800f32,
+                x: 800f32,
+                y: 600f32,
             }),
             ..Default::default()
         },
