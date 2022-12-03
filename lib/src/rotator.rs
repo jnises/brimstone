6250cf3898342ebea5061d4e88a099a65a8b4cd6
Joel Nises <joel.nises@gmail.com>
2022-12-03 22:06:58 +0100

    fixed rotator widget

--- lib/src/rotator.rs
+++ lib/src/rotator.rs
@@ -5,7 +5,7 @@ use eframe::{
     emath,
     epaint::{Color32, Stroke},
 };
-use glam::{Quat, Vec3};
+use glam::{vec2, vec3, Quat, Vec3, Vec3Swizzles};
 use palette::convert::FromColorUnclamped;
 
 pub struct Rotator<'a> {
@@ -28,12 +28,16 @@ impl<'a> Widget for Rotator<'a> {
         let id = response.id;
         // TODO is this correct?
         let on = if let Some(pos) = response.interact_pointer_pos() {
-            let data = *ui.data().get_temp_mut_or_insert_with(id, || State {
-                start_rot: *self.quat,
-                start_pos: pos,
-            });
+            let data = ui
+                .data()
+                .get_temp_mut_or_insert_with(id, || State {
+                    start_rot: *self.quat,
+                    start_pos: pos,
+                })
+                .clone();
             let d = pos - data.start_pos;
-            *self.quat = Quat::from_scaled_axis(glam::vec3(d.y, -d.x, 0.) * 0.01) * data.start_rot;
+            // 2d cross product, with flipped y
+            *self.quat = Quat::from_scaled_axis(glam::vec3(d.y, d.x, 0.) * 0.01) * data.start_rot;
             true
         } else {
             ui.data().remove::<State>(id);
@@ -49,23 +53,27 @@ impl<'a> Widget for Rotator<'a> {
                 visuals.bg_fill,
                 visuals.fg_stroke,
             );
-            let mut axis = [(Vec3::X, 0.), (Vec3::NEG_Y, 120.), (Vec3::NEG_Z, 240.)]
+            let mut axis = [(Vec3::X, 0.), (Vec3::Y, 120.), (Vec3::Z, 240.)]
                 .into_iter()
                 .map(|(v, h)| (v, h, *self.quat * v))
                 .collect::<Vec<_>>();
-            axis.sort_by(|(_, _, Vec3 { z: az, .. }), (_, _, Vec3 { z: bz, .. })| {
-                bz.partial_cmp(az).unwrap_or(Ordering::Equal)
-            });
+            axis.sort_by(|(_, _, a), (_, _, b)| a.z.partial_cmp(&b.z).unwrap_or(Ordering::Equal));
+            let camera = glam::Mat4::look_at_rh(vec3(0.0, 0.0, 2.0), Vec3::ZERO, Vec3::Y);
+            let proj = glam::Mat4::perspective_rh(std::f32::consts::FRAC_PI_3, 1.0, 1.0, 3.0);
+            let view_proj = proj * camera;
             for (_, h, rv) in axis {
-                let depth = 1. - ((rv.z + 1.) / 2.).clamp(0., 1.);
+                let projected = view_proj.project_point3(rv);
+                let depth = 1.0 - projected.z;
                 let c = palette::Srgb::from_color_unclamped(palette::Oklch {
                     l: 0.4 + depth * 0.4,
                     chroma: 0.3 + depth * 0.2,
                     hue: h.into(),
                 })
                 .into_format();
-                let p =
-                    rect.center() + emath::vec2(rv.x, rv.y) / (1.7 - depth) * rect.width() / 2.1;
+                let p = rect.center()
+                    + emath::Vec2::from((projected.xy() * vec2(1.0, -1.0)).to_array())
+                        * rect.width()
+                        / 2.1;
                 painter.line_segment(
                     [rect.center(), p],
                     Stroke {
