ce2fb33fe247bf9c3bbeec3991e2d653edd2fa32
Joel Nises <joel.nises@gmail.com>
2024-02-24 21:02:34 +0100

    update deps

--- standalone/src/main.rs
+++ standalone/src/main.rs
@@ -1,17 +1,10 @@
-use eframe::egui::Vec2;
 use lib::Gui;
 
 fn main() -> eframe::Result<()> {
     env_logger::init();
     eframe::run_native(
         "Brimstone",
-        eframe::NativeOptions {
-            initial_window_size: Some(Vec2 {
-                x: 800f32,
-                y: 600f32,
-            }),
-            ..Default::default()
-        },
+        eframe::NativeOptions::default(),
         Box::new(|_ctx| Box::<Gui>::default()),
     )
 }
